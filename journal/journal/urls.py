e36df8f546f6139a795f663ee400ca616ff46938
Anastasiia <a.dol4356@gmail.com>
2021-10-04 00:13:40 +0300

    test webhook

--- journal/journal/urls.py
+++ journal/journal/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('register/', include('register.urls')),
     path('', include('main.urls'))
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG:
+#    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
